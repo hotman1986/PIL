d515fae37d48229426ee9c38cfaff2b2fdb807fa
hotman1986 <hongxiang.tang@studenti.unicam.it>
2017-08-30 21:43:24 +0200

    Update imtool.py

--- excise/imtool.py
+++ excise/imtool.py
@@ -1,8 +1,13 @@
-
-
-
 def imresize(im,sz):
     pil_im = Image.fromarray(uint8(im))
     return array(pil_im.resize(sz))
 
-def his
+
+#cumulative distribution function,对直方图均衡化
+def histeq(im,nbr_bins=256):
+    imhist,bins = histogram(im.flatten(),nbr_bins,normed = True)
+    cdf = imhist.cumsum()
+    cdf = 255 * cdf[-1] #归一化
+    
+    im2 = interp(im.flatten(),bins[:-1],cdf)
+    return im2.reshape(im.shape),cdf
